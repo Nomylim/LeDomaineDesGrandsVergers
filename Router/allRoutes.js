990101d42a23dca684de16ce6b084ee2c49b9a86
Nomy <noemielimbasse@gmail.com>
2024-09-04 19:33:13 +0200

    Création et codage de la page atelier

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/l'atelier", "l'atelier", "/pages/l'atelier.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Domaine des grands Vergers";
