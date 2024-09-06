670f90904adc2b67b01c126e37a5229c82aab057
Nomy <noemielimbasse@gmail.com>
2024-09-06 13:28:28 +0200

    ajout font et vérification de toutes les pages pour les espaces en trop ou manquant et vérification des liens / boutons

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,11 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/l'atelier", "l'atelier", "/pages/l'atelier.html"),
-    new Route("/pommiers", "les pommiers", "/pages/pommiers.html"),
-    new Route("/tarifs", "les tarifs", "/pages/tarifs.html"),
-    new Route("/pechers", "les pêchers", "/pages/pechers.html"),
-    new Route("/cerisiers", "les cerisiers", "/pages/cerisiers.html"),
+    new Route("/l'atelier", "L'atelier", "/pages/l'atelier.html"),
+    new Route("/pommiers", "Les Pommiers", "/pages/pommiers.html"),
+    new Route("/tarifs", "Les Tarifs", "/pages/tarifs.html"),
+    new Route("/pechers", "Les Pêchers", "/pages/pechers.html"),
+    new Route("/cerisiers", "Les Cerisiers", "/pages/cerisiers.html"),
 
 ];
 
